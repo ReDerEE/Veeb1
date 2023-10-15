9c58a8a9f1638f980f666c2fb03884e51f0755d5
Carl-Eric Sepp <carsepp@greeny.cs.tlu.ee>
2023-10-15 12:39:25 +0300

    Tunnitööd tehtud

---
+++ dateTime_ET.js
@@ -0,0 +1,36 @@
+exports.dateETformatted = function(){
+    const monthNamesET = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"];
+    let timeNow = new Date();
+    let dateNow = timeNow.getDate();
+    let monthNow = timeNow.getMonth();
+    let yearNow = timeNow.getFullYear();
+    let dateET = dateNow+". "+monthNamesET[monthNow]+" "+yearNow;
+    return dateET;
+}
+exports.timeFormatted = function(){
+    let timeNow = new Date();
+    let timeFormatted = (timeNow.getHours()+":"+timeNow.getMinutes()+":"+timeNow.getSeconds());
+    return timeFormatted;
+}
+//module.exports = (dateETformatted: dateETformatted, timeFormatted: timeFormatted);
+
+exports.timeOfDayET = function(){
+    let partOfDay = "suvaline hetk";
+    let hourNow = new Date().getHours();
+    if(hourNow >= 18){
+        partOfDay = "õhtu"
+    }
+    if(hourNow >= 6 && hourNow < 12){
+        partOfDay = "hommik"
+    }
+    if(hourNow > 12 && hourNow < 18){
+        partOfDay = "pärasthommik"
+    }
+    return partOfDay
+}
+
+exports.formatTime = function(day, month, year){
+    const monthNamesET = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"];
+    let formattedTime = (day+". "+monthNamesET[month]+" "+year);
+    return formattedTime;
+};
--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const express = require('express');
 const app = express();
+const timeInfo = require("./dateTime_ET.js");
+const fs = require("fs");
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
     res.render('index');
 });
 
-app.get('/test', (req, res)=>{
-    res.send('test worked')
+app.get('/timenow', (req, res)=>{
+    const dateNow = timeInfo.dateETformatted();
+    const timeNow = timeInfo.timeFormatted();
+    res.render('timenow', {nowD: dateNow, nowT: timeNow});
+    
+});
+
+app.get('/wisdom', (req, res)=>{
+    let folkWisdoms = [];
+    fs.readFile('public/txtFiles/vanasonad.txt', 'utf-8',(err, data)=>{
+        if (err){
+            throw err;
+        }
+        else {
+            folkWisdoms = data.split(';');
+            res.render('justlist', {h1: 'Vanasõnad', wisdom: folkWisdoms});
+        }
+    });
+    //res.render('wisdom');
+});
+app.get('/names', (req, res)=>{
+    let nameDateArray = []
+    let nameArray = []
+    fs.readFile('public/txtFiles/nameLog.txt', 'utf-8', (err, data)=>{
+        if (err){
+            throw err;
+        }
+        else{
+            nameDateArray = data.split(";")
+            for(i in nameArray){
+                nameDateArray[i]=nameDateArray[i].replace(",", " ");
+            nameDateArray.split(",");
+            for(i in nameArray){
+                nameArray.push()
+            }
+            }
+            nameArray.pop(-1);
+            res.render('namePage', {names: nameArray, h1: 'Inimesed, kes on külastanud'})
+            
+        }
+    })
 });
 
 app.listen(5132);
